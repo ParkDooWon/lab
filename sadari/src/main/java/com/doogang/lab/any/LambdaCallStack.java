3e71fd4569492faa27c18f74891f3e9a389938de
ParkDooWon <pass345@naver.com>
2024-02-14 17:36:59 +0900

    java lambda test

--- sadari/src/main/java/com/doogang/lab/any/LambdaCallStack.java
+++ sadari/src/main/java/com/doogang/lab/any/LambdaCallStack.java
@@ -1,11 +1,31 @@
 package com.doogang.lab.any;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+import com.doogang.lab.user.domain.User;
 
 public class LambdaCallStack {
 
   public Runnable getRunnable() {
     AtomicInteger cnt = new AtomicInteger();
+    int num = 10;
+    Consumer<Integer> consumer = System.err::println;
+    consumer.accept(num);
+    num = 1000;
     return () -> cnt.getAndIncrement();
+  }
+
+  public Runnable test(User user) {
+    User user1 = new User();
+    System.err.println(Thread.currentThread().getName() + " : " + user1);
+    return () -> {
+      // System.err.println("Thread name : " + Thread.currentThread().getName() + " : " );
+      // System.err.println("Thread name : " + Thread.currentThread().getName() + " : " + user1);
+      System.err.println("Thread name : " + Thread.currentThread().getName() + " : " + user1.getName() + " / reference : " + user1);
+      // System.err.println(user);
+      user1.setName("ddd");
+      // System.err.println("now name : " + user1.getName());
+    };
   }
 }
--- sadari/src/main/java/com/doogang/lab/any/TestMain.java
+++ sadari/src/main/java/com/doogang/lab/any/TestMain.java
@@ -1,16 +1,57 @@
 package com.doogang.lab.any;
 
+import com.doogang.lab.user.domain.User;
+
 public class TestMain {
   public static void main(String[] args) {
     LambdaCallStack lambdaCallStack1 = new LambdaCallStack();
-    Runnable runnable1 = lambdaCallStack1.getRunnable();
-    runnable1.run();
-    runnable1.run();
-    runnable1.run();
+    User user = new User();
+    // Thread thread1 = new Thread(lambdaCallStack1.test(user), "first");
+    // Thread thread2 = new Thread(lambdaCallStack1.test(user), "second");
+    // Thread thread3 = new Thread(lambdaCallStack1.test(user), "third");
+    // Thread thread4 = new Thread(lambdaCallStack1.test(user), "forth");
+    // Thread thread41 = new Thread(lambdaCallStack1.test(user), "1");
+    // Thread thread42 = new Thread(lambdaCallStack1.test(user), "2");
+    // Thread thread43 = new Thread(lambdaCallStack1.test(user), "3");
+    // Thread thread44 = new Thread(lambdaCallStack1.test(user), "4");
+    // Thread thread45 = new Thread(lambdaCallStack1.test(user), "5");
+    // Thread thread46 = new Thread(lambdaCallStack1.test(user), "6");
 
-    LambdaCallStack lambdaCallStack2 = new LambdaCallStack();
-    Runnable runnable2 = lambdaCallStack2.getRunnable();
-    runnable2.run();
-    runnable2.run();
+    Runnable test = lambdaCallStack1.test(user);
+    for (int i=0;i<100;i++) {
+      test.run();
+    }
+
+    // Thread thread1 = new Thread(lambdaCallStack1.test(), "first");
+    // Thread thread2 = new Thread(lambdaCallStack1.test(), "second");
+    // Thread thread3 = new Thread(lambdaCallStack1.test(), "third");
+    // Thread thread4 = new Thread(lambdaCallStack1.test(), "forth");
+    // Thread thread41 = new Thread(lambdaCallStack1.test(), "1");
+    // Thread thread42 = new Thread(lambdaCallStack1.test(), "2");
+    // Thread thread43 = new Thread(lambdaCallStack1.test(), "3");
+    // Thread thread44 = new Thread(lambdaCallStack1.test(), "4");
+    // Thread thread45 = new Thread(lambdaCallStack1.test(), "5");
+    // Thread thread46 = new Thread(lambdaCallStack1.test(), "6");
+
+    // thread1.start();
+    // thread2.start();
+    // thread3.start();
+    // thread4.start();
+    // thread41.start();
+    // thread42.start();
+    // thread43.start();
+    // thread44.start();
+    // thread45.start();
+    // thread46.start();
+
+    // Runnable runnable1 = lambdaCallStack1.getRunnable();
+    // runnable1.run();
+    // runnable1.run();
+    // runnable1.run();
+    //
+    // LambdaCallStack lambdaCallStack2 = new LambdaCallStack();
+    // Runnable runnable2 = lambdaCallStack2.getRunnable();
+    // runnable2.run();
+    // runnable2.run();
   }
 }
